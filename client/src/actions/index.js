378f98c6667c60164943d8eb2616a2e425edfeae
sibalajp <sibalajp@gmail.com>
2018-01-27 18:41:19 -0800

    actions update

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,14 +4,14 @@ import { FETCH_USER	} from './types';
 export const fetchUser = () => {
 	return function(dispatch) {
 		axios.get('/api/current_user')
-		.then( res => dispatch({ type: FETCH_USER, payload: res.data })); // once promise is done, dispatch action send payload to the reducers
+		.then( res => dispatch({ type: FETCH_USER, payload: res.data })); 
 	} 
 }
 
 export const handleToken = (token) => async dispatch => {
 	const res = await axios.post('/api/stripe', token);
 
-	dispatch({ type: FETCH_USER, payload: res.data }); // using the same action type to update the user model
+	dispatch({ type: FETCH_USER, payload: res.data }); 
 }
 
 
